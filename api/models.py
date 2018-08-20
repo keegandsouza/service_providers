9cb6e3caa8d6c8175584cd10c5465b1def653242
Keegan Dsouza <keegangoa@gmail.com>
2018-08-20 13:51:48 +0530

    initial commit

---
+++ api/models.py
@@ -0,0 +1,41 @@
+from django.contrib.gis.db import models
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=15, null=False)
+    symbol = models.CharField(max_length=1, null=False, default="")
+    abbreviation = models.CharField(max_length=5, null=False, default="")
+
+    def __str__(self):
+        return self.name
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=20)
+    language = models.ForeignKey(Language, null=True)
+    currency = models.ForeignKey(Currency, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ServiceArea(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    polygon = models.PolygonField(null=True, blank=True)
+    provider = models.ForeignKey(Provider, null=False)
+
+    objects = models.GeoManager()
+
+    def __str__(self):
+        return self.name
---
+++ api/resources.py
@@ -0,0 +1,43 @@
+from tastypie import fields
+from tastypie.resources import ALL
+from tastypie.contrib.gis.resources import ModelResource
+from tastypie.authorization import Authorization
+from .models import Provider, ServiceArea, Language, Currency
+
+
+class LanguageResource(ModelResource):
+    class Meta:
+        queryset = Language.objects.all()
+        resource_name = 'language'
+
+
+class CurrencyResource(ModelResource):
+    class Meta:
+        queryset = Currency.objects.all()
+        resource_name = 'currency'
+
+
+class ProviderResource(ModelResource):
+    currency = fields.ToOneField(CurrencyResource, attribute='currency', null=True, full=True)
+    language = fields.ToOneField(LanguageResource, attribute='language', null=True, full=True)
+
+    class Meta:
+        queryset = Provider.objects.all()
+        resource_name = 'provider'
+        authorization = Authorization()
+               
+
+
+
+class ServiceAreaResource(ModelResource):
+    provider = fields.ToOneField(ProviderResource, attribute='provider', null=True, full=True)
+
+    class Meta:
+        queryset = ServiceArea.objects.all()
+        resource_name = 'service_area'
+        authorization = Authorization()
+        filtering = {
+            'polygon': ALL,
+        }
+
+
